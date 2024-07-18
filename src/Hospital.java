81a8b87fefcd622a2a0f151efb1f973b64b9a4fd
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-07-18 11:16:30 -0400

    finish with inheritance example

---
+++ src/Hospital.java
@@ -0,0 +1,10 @@
+public class Hospital {
+    public static void main(String[] args) {
+
+        //System.out.println("Hello world!");
+        Surgeon s = new Surgeon();
+        s.Doctor_Details();
+        s.Surgeon_Details();
+
+    }
+}
